fb461b9895de51ac6bc8e847493bf827966b3e2c
Pedro Goldberg <safestudies@tuta.io>
2023-10-28 21:23:55 +0300

    nested loop for  mario.c file for ?

--- week_1/lesson_1/mario.c
+++ week_1/lesson_1/mario.c
@@ -2,5 +2,12 @@
 
 int main(void)
 {
-    printf("????\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
