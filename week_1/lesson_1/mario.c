854bce2aee2b4e6582e7090faef5c854ccf7c4f5
Pedro Goldberg <safestudies@tuta.io>
2023-10-28 22:03:48 +0300

    added do/while loop mario.c file for ?

--- week_1/lesson_1/mario.c
+++ week_1/lesson_1/mario.c
@@ -1,8 +1,15 @@
-#include "stdio.h"
+#include <stdio.h>
+#include "cs50.h"
 
 int main(void)
 {
-    const int n = 3;
+    int n;
+    do
+    {
+        n = get_int("size: ");
+    } 
+    while (n < 1);
+    
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
