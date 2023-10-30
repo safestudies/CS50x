f272546d53156956a68a501a2c7aac55d98e40f4
Pedro Goldberg <safestudies@tuta.io>
2023-10-30 12:16:00 +0200

    added revised version for lab1

---
+++ week_1/lesson_1/population2.0.c
@@ -0,0 +1,54 @@
+// 30-10-23
+#include "cs50.h"
+#include <stdio.h>
+
+double get_startsize(void);
+double get_endsize(double start);
+// void print_years(int years)
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    // s = start population
+    double s = get_startsize();
+
+    // TODO: Prompt for end size
+    // e = end population
+    double e = get_endsize(s);
+
+    // TODO: Calculate number of years until we reach threshold
+    // y = number of years | n = next years population
+    int y = 0;
+    double n = s;
+
+    while (n < e)
+    {
+        n = n + (n / 3.0) - (n / 4.0);
+        y++;
+    }
+
+    // TODO: Print number of years
+    printf("Years: %i\n", y);
+}
+
+double get_startsize(void)
+{
+    double s;
+    do
+    {
+        s = get_double("start size: ");
+    }
+    while (s < 9.0);
+    return s;
+}
+
+double get_endsize(double start)
+{
+    double e;
+    do
+    {
+        e = get_double("end size: ");
+    }
+    while (e < start);
+    return e;
+}
