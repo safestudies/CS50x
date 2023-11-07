e6e30a946eee7f4c969e80930a85763f247da394
safestudies <safestudies@tuta.io>
2023-11-07 12:54:51 +0200

    ScoresAverage_array.c

---
+++ week_2/ScoresAverage_array.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "cs50.h"
+
+float average(int tests, int scores[]);
+
+int main(void)
+{
+    // Getting scores from x tests:
+    const int tests = 3;
+    int scores[tests];
+
+    for (int i = 0; i < tests; i++)
+    {
+        scores[i] = get_int("test score: ");
+    }
+
+    for (int i = 0; i < tests; i++)
+    {
+        printf("score: %i\n", scores[i]);
+    }
+
+    printf("Average: %.2f\n", average(tests, scores));
+}
+
+float average(int tests, int scores[])
+{
+    int sum = 0;
+
+    for (int i = 0; i < tests; i++)
+    {
+        sum += scores[i];
+    }
+    return (float) sum / tests;
+}
